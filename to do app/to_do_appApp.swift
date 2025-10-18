2a79f6cfcb83d5975828389061a3ea7782a5d618
bektasing <bektashamza001@gmail.com>
2025-10-18 23:43:59 +0300

    Initial Commit

---
+++ to do app/to_do_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_appApp.swift
+//  to do app
+//
+//  Created by Macbook Air on 18.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct to_do_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
